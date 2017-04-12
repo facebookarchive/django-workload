e2f00ce320154408c0ec6359b42dcf54a5d22561
Martijn Pieters <mjpieters@fb.com>
2017-04-12 09:38:45 -0700

    Patch the cassandra driver to record timings.

--- django-workload/django_workload/patches.py
+++ django-workload/django_workload/patches.py
@@ -1,10 +1,21 @@
+from functools import wraps
+from inspect import getdoc
+
 from statsd.defaults import django as statsd_django_defaults
 from statsd.client import StatsClient
 
 
-# Patch up the django_statsd client handling (pass in ipv6 flag)
+_patches = []
+
+
+def register_patch(f):
+    _patches.append((f, getdoc(f) or ''))
+
+
 # TODO: send a pull request upstream
+@register_patch
 def patch_django_statsd_ipv6():
+    """Make django_statsd work with IPv6"""
     def insert_ipv6(**kwargs):
         if 'ipv6' not in kwargs:
             kwargs['ipv6'] = statsd_django_defaults.ipv6
@@ -25,5 +36,22 @@ def patch_django_statsd_ipv6():
             raise
 
 
+@register_patch
+def patch_cassandra_execute():
+    """Record timings for Cassandra operations"""
+    from django_statsd.clients import statsd
+    from django_cassandra_engine.connection import CassandraConnection
+
+    def decorator(orig):
+        @wraps(orig)
+        def timed_execute(self, *args, **kwargs):
+            with statsd.timer('cassandra.execute'):
+                return orig(self, *args, **kwargs)
+
+    CassandraConnection.execute = decorator(CassandraConnection.execute)
+
+
 def apply():
-    patch_django_statsd_ipv6()
+    for patch, descr in _patches:
+        print(descr)
+        patch()
