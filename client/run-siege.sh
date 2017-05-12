0c7457926d22225600ac670c5a461561448c3a43
Florin Papa <florin.papa@intel.com>
2017-05-12 13:57:37 +0100

    Add script for running Siege

---
+++ client/run-siege.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Florin Papa <florin.papa@intel.com>
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+# run siege benchmark
+# -c specifies the number of workers
+# -b tells siege to enter benchmarking mode
+# -t 120S specifies the run time of the benchmark, in this case 120 seconds
+# -f urls.txt tells siege which urls to benchmark
+siege -c 144 -b -t 120S -f urls.txt
