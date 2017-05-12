af74d8010018e4e38652108402404790ac5a910e
Martijn Pieters <mjpieters@fb.com>
2017-05-12 13:57:37 +0100

    Make siege parameters configurable

--- client/run-siege.sh
+++ client/run-siege.sh
@@ -3,10 +3,22 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+# Concurrent worker count
+WORKERS="${WORKERS:-144}"
+
+# Test duration (units are H, M or S, for hours, minutes or seconds)
+DURATION="${DURATION:-2M}"
+
+# Log file (outputs CSV rows)
+LOG="${LOG:-./siege.log}"
+
+# Source file
+SOURCE="${SOURCE:-urls.txt}"
+
 # run siege benchmark
 # -c specifies the number of workers
 # -b tells siege to enter benchmarking mode
 # -t 120S specifies the run time of the benchmark, in this case 120 seconds
 # -f urls.txt tells siege which urls to benchmark
 # --log=./siege.log specifies the output file
-siege -c 144 -b -t 120S -f urls.txt --log=./siege.log
+siege -c ${WORKERS} -b -t ${DURATION} -f ${SOURCE} --log=${LOG}
